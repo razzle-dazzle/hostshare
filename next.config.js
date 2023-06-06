21297710623bd5d52a23dd6384495ef40b5368eb
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2023-06-06 17:31:01 +0200

    Added sass

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
       'a0.muscache.com'
     ]
-  }
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
 }
 
 module.exports = nextConfig
